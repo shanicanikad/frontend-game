53d1d9a60007c1c4e72fd59e8479e6e8fc837793
SHANICA_DENIS <shanicanikad@gmail.com>
2020-10-16 14:22:28 -0400

    final

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # frontend-game
+
+Flash-Cards
+This flash card game is loaded with some data, and let's the user flip through the flash cards quickly (back or front). The user needs to just hover over the flashcard for it to flip and self record whether he or she guessed right or wrong. Let's see if you're a true 'Attack On Titans' fan!
--- script.js
+++ script.js
@@ -6,8 +6,15 @@ let button = document.querySelector(".button")
 let start = document.querySelector(".start")
 let dropdownContent = document.querySelector(".dropdown-content")
 let dropbtn = document.querySelector(".dropbtn")
-let correct= document.querySelectorAll(".correctButton")
-let incorrect= document.querySelectorAll(".incorrectButton")
+let correct= document.querySelector(".correct")
+let incorrect= document.querySelector(".incorrect")
+
+
+let pointsCorrect = correct.innerText
+let pointsIncorrect = incorrect.innerText
+pointsCorrect = 0
+pontsIncorrect = 0 
+
 
 whole.style.opacity = 0;
 button.addEventListener("click", function (){
@@ -25,6 +32,9 @@ for (let i = 0; i < flashcardInside.length; i++){
     let div = document.createElement("div");
     let button1 = document.createElement("button")
     let button2 = document.createElement("button")
+    button1.classList.add("correctButton")
+    button2.classList.add("incorrectButton")
+
     
     button1.innerText = "Good job! 😄 😜 🥳"
     button2.innerText = "Your not a true fan! 😠 😤 🤬 " 
@@ -33,33 +43,37 @@ for (let i = 0; i < flashcardInside.length; i++){
     div.classList.add("goodOrBad")
     button1.classList.add("correctButton")
     button2.classList.add("incorrectButton")
+    button1.addEventListener("click", function(e){  
+        pointsCorrect +=10
+        correct.innerText = pointsCorrect 
+    })  
 
-
+    button2.addEventListener("click", function(e){
+        pointsIncorrect -=10
+        incorrect.innerText = pointsIncorrect
+        })
+    
     flashcardInside[i].appendChild(div)   
     let divs = document.querySelectorAll(".goodOrBad")
     divs[i].appendChild(button1)
     divs[i].appendChild(button2)
-}
+    }
 
 // let pointsCorrect = correct.innerText
 // let pointsIncorrect = incorrect.innerText
-// pointsCorrect = 0
-// pointsIncorrect = 0 
+// correct = 0
+// incorrect = 0 
 
-for (let i = 0; i < correct.length; i++){
-    correct[i].addEventListener("click", function(e){
-        console.log("this works")
-        let pointsCorrect = correct.innerText
-        let pointsIncorrect = incorrect.innerText
-        // pointsCorrect = 0
-        // pointsIncorrect = 0 
-        pointsCorrect +=10
-    })   
-}  
+// for (let i = 0; i < correct.length; i++){
+//     button1[i].addEventListener("click", function(e){  
+//         pointsCorrect +=10
+//         correct.innerText = pointsCorrect 
+//     })   
+// }  
 
-for (let i = 0; i < incorrect.length; i++){
-    incorrect[i].addEventListener("click", function(e){
-    pointsIncorrect -=10
-    })
-}
+// for (let i = 0; i < incorrect.length; i++){
+//     button2[i].addEventListener("click", function(e){
+//     pointsIncorrect -=10
+//     })
+// }
 
